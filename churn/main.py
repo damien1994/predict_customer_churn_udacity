a554875df32331f7086cea814b81f05090f72e52
damien <dmichelle@efficity.com>
2021-08-05 23:16:05 +0200

    global refactoring - make main works

--- churn/main.py
+++ churn/main.py
@@ -1,13 +1,42 @@
 import logging as lg
 
-from churn.config import IMAGES_EDA_OUTPUT_DIR, IMAGES_EDA_SUB_DIR, NUM_COLUMNS, CAT_COLUMNS, TARGET_COL
-from churn.utils import import_data, perform_eda
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
+from churn.config import IMAGES_EDA_OUTPUT_DIR, IMAGES_EDA_SUB_DIR, MODEL_OUTPUT_DIR, \
+    RESULTS_OUTPUT_DIR, NUM_COLUMNS, CAT_COLUMNS, TARGET_COL, PARAM_GRID, ENCODING_TARGET
+from churn.utils import import_data, perform_eda, encoder_helper, perform_feature_engineering, \
+    train_models, evaluate_model
 
 
 def main():
+    lg.info('Import data')
     df = import_data("data/bank_data.csv", NUM_COLUMNS, CAT_COLUMNS, TARGET_COL)
     lg.info(df.shape)
-    perform_eda(df, NUM_COLUMNS, CAT_COLUMNS, TARGET_COL[0], IMAGES_EDA_OUTPUT_DIR, IMAGES_EDA_SUB_DIR)
+    lg.info('Perform EDA')
+    #perform_eda(df, NUM_COLUMNS, CAT_COLUMNS, TARGET_COL[0], IMAGES_EDA_OUTPUT_DIR, IMAGES_EDA_SUB_DIR)
+    lg.info('Perform basic encoding')
+    df = encoder_helper(df, 'Attrition_Flag', 'Churn', ENCODING_TARGET, CAT_COLUMNS)
+    print(df.shape)
+    lg.info('Split data into train/test and x/y datasets')
+    (X_train, X_test, y_train, y_test), feature_names = perform_feature_engineering(df, 'Churn', 0.3, 42)
+    print(X_train.shape, X_test.shape, len(y_train), len(y_test))
+    lg.info('Perform modelisation')
+    lrc = LogisticRegression(max_iter=200)
+    rfc = RandomForestClassifier(random_state=42)
+    for model in [lrc, rfc]:
+        if model == lrc:
+            fitted_model, predictions = train_models(X_train, X_test, y_train, model, output_dir=MODEL_OUTPUT_DIR)
+        elif model == rfc:
+            fitted_model, predictions = train_models(X_train, X_test, y_train, model, param_grid=PARAM_GRID,
+                                                     cv=5, grid_search=True, do_probabilities=False,
+                                                     output_dir=MODEL_OUTPUT_DIR)
+        lg.info('Perform model evaluation')
+        # TO DO : insert an argument to choose if you want to train model or load their
+        # pickle format
+        #fitted_model = lrc.fit(X_train, y_train)
+        #predictions = fitted_model.predict(X_test)
+        evaluate_model(fitted_model, X_train, X_test, y_test, predictions, RESULTS_OUTPUT_DIR)
 
 
 if __name__ == '__main__':
--- churn/utils.py
+++ churn/utils.py
@@ -2,10 +2,13 @@
 File to store all functions for customer churn predictions
 """
 import os
+import shap
 import logging as lg
 import pandas as pd
+import numpy as np
+import joblib
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
+import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
@@ -54,34 +57,26 @@ def perform_eda(df: pd.DataFrame, num_cols: list, cat_cols: list, target_col: st
     plot_target_distribution(df, target_col, output_dir)
 
 
-def encode_target(df: pd.DataFrame, target_name: str, target_encoding: dict) -> dict:
+def encoder_helper(df: pd.DataFrame, target_name: str, new_target_name: str, target_encoding_dict: dict, cat_feature: list) -> pd.DataFrame:
     """
-    Encode target variable
+    Encode target and categorical features
     :param df: a pandas dataframe
     :param target_name: the target column to encode
-    :param target_encoding: how to encode the target value
-    :return: a dictionary with <new_encode : value to be encoded>
-    """
-    for key, value in target_encoding.items():
-        return {key: df[target_name] == value}
-
-
-def encoder_helper(df: pd.DataFrame, feature: str) -> pd.DataFrame:
-    """
-    Feature engineering - compute a target encoding
-    :param df: the pandas dataframe
-    :param feature: a column from the dataframe
-    :return: a pandas dataframe with the target encoding to the feature
+    :param new_target_name: name of the target col encoded
+    :param target_encoding_dict: how to encode the target value
+    :param cat_feature: list of categorical columns
+    :return: a pandas dataframe with the target and categorical column encoded
     """
     try:
-        feature_groups = df.groupby(feature).mean()['Churn']
-        df['{0}_Churn'.format(feature)] = df[feature].map(feature_groups)
-        return df
+        dataframe = encode_target(df, target_name, new_target_name, target_encoding_dict)
+        for col in cat_feature:
+            encoder_cat_features(dataframe, col)
+        return dataframe
     except:
         lg.info("Something went wrong")
 
 
-def perform_feature_engineering(df, target, test_size, random_state):
+def perform_feature_engineering(df: pd.DataFrame, target: str, test_size: float, random_state: int):
     """
     Split data into train and test labels
     :param df: a pandas dataframe
@@ -90,30 +85,13 @@ def perform_feature_engineering(df, target, test_size, random_state):
     :param random_state: controls the randomness of the sampled classes
     :return: data and labels for train and test samples - return pandas dataframes & series
     """
-    return train_test_split(df.drop(target, axis=1), df[target], test_size=test_size, random_state=random_state)
-
-
-def classification_report_image(true_labels, predictions, name_model, mode, output_path):
-    """
-    produces classification report for training and testing results and stores report as image
-    in images folder
-    :param true_labels:
-    :param predictions:
-    :param name_model:
-    :output_path:
-    :return:
-    """
-    try:
-        lg.info(f'{name_model} results')
-        if mode == 'train':
-            lg.info(f'{mode} results')
-            return classification_report(true_labels, predictions)
-    except:
-        lg.info('Something went from')
+    df = df.select_dtypes(exclude=['object'])
+    return train_test_split(df.drop(target, axis=1), df[target], test_size=test_size, random_state=random_state), \
+        df.drop(target, axis=1).columns
 
 
 def train_models(x_train_data, x_test_data, y_train_data, model, param_grid=None, cv=None,
-                 grid_search=False, do_probabilities=False):
+                 grid_search=False, do_probabilities=False, output_dir=None):
     try:
         if grid_search:
             gs = GridSearchCV(
@@ -127,24 +105,30 @@ def train_models(x_train_data, x_test_data, y_train_data, model, param_grid=None
         else:
             fitted_model = model.fit(x_train_data, y_train_data)
 
+        if output_dir:
+            save_model(fitted_model, output_dir)
+
         predictions = fitted_model.predict_proba(x_test_data) if do_probabilities else fitted_model.predict(x_test_data)
         return fitted_model, predictions
     except:
         lg.info('complicated life')
 
 
-def plot_roc_curves(model, true_labels, predictions):
-    """
-    Plot roc curve associated to the model
-    :param model:
-    :param true_labels:
-    :param predictions:
-    :return:
-    """
-    try:
-        return plot_roc_curve(model, true_labels, predictions)
-    except:
-        lg.info('try something')
+def evaluate_model(model_fitted, train_data, test_data, true_labels, predictions, output_dir):
+    if isinstance(model_fitted, GridSearchCV):
+        model_name = type(model_fitted.best_estimator_).__name__
+        coefficients = model_fitted.best_estimator_.feature_importances_
+    else:
+        model_name = type(model_fitted).__name__
+        coefficients = model_fitted.coef_[0]
+    safe_creation_directory(output_dir)
+    store_classification_report(true_labels, predictions, model_name, output_dir)
+    store_model_roc_curve(model_fitted, test_data, true_labels, model_name, output_dir)
+    compute_shapley_values(model_fitted, test_data, output_dir, model_name, train_data)
+    #if model_fitted.coef_.all:
+    #    coefficients = model_fitted.coef_[0]
+    #coefficients = model_fitted.coef_[0] if model_fitted.coef_.all else model_fitted.best_estimator_.feature_importances_
+    compute_features_importances(train_data.columns, coefficients, model_name, output_dir)
 
 
 def safe_creation_directory(path):
@@ -226,3 +210,122 @@ def plot_target_distribution(df: pd.DataFrame, target: str, output_dir: str):
     plt.title('target distribution')
     target_distribution = df[target].value_counts('normalize').plot(kind='bar')
     plt.savefig(f'{output_dir}/target/target_distribution.png', bbox_inches='tight')
+
+
+def encoder_cat_features(df: pd.DataFrame, feature: str) -> pd.DataFrame:
+    """
+    Feature engineering - compute a target encoding
+    :param df: the pandas dataframe
+    :param feature: a column from the dataframe
+    :return: a pandas dataframe with the target encoding to the feature
+    """
+    try:
+        feature_groups = df.groupby(feature).mean()['Churn']
+        df['{0}_Churn'.format(feature)] = df[feature].map(feature_groups)
+        return df
+    except:
+        lg.info("Something went wrong")
+
+
+def compute_encoding_target_dict(df: pd.DataFrame, target_name: str, target_encoding_dict: dict) -> dict:
+    """
+    Compute dictionary for encoding target
+    :param df: a pandas dataframe
+    :param target_name: the target column to encode
+    :param target_encoding_dict: how to encode the target value
+    :return: a dictionary with <new_encode : value to be encoded>
+    """
+    for key, value in target_encoding_dict.items():
+        return {key: df[target_name] == value}
+
+
+def encode_target(df: pd.DataFrame, target_name: str, new_target_name: str, target_encoding_dict: dict) -> pd.DataFrame:
+    """
+    Encode target variable
+    :param df: a pandas dataframe
+    :param target_name: the target column to encode
+    :param new_target_name: name of the target col encoded
+    :param target_encoding_dict: how to encode the target value
+    :return: a pandas series that corresponds to the new target column encoded
+    """
+    try:
+        encoding_dict = compute_encoding_target_dict(df, target_name, target_encoding_dict)
+        df[new_target_name] = np.select(encoding_dict.values(), encoding_dict.keys(), default=1)
+        return df.drop(target_name, axis=1)
+    except KeyError as error:
+        lg.info('something went wrong')
+        raise error
+
+
+def save_model(model, output_dir: str):
+    """
+    Save the model fitted into a .pkl file
+    :param model: model fitted - an scikit learn object in our case
+    :param output_dir: path where to store the model
+    :return a .pkl into the output path
+    """
+    safe_creation_directory(output_dir)
+    return joblib.dump(model, f'{output_dir}/{type(model).__name__}.pkl')
+
+
+def load_model(input_path: str):
+    """
+    Load a .pkl file into a model
+    :param input_path: path where the model is stored
+    :return a a scikit learn model (in our case - can return other objects)
+    """
+    return joblib.load(input_path)
+
+
+def store_classification_report(true_labels, predictions, model_name, output_dir):
+    report = classification_report(true_labels, predictions, output_dict=True)
+    return pd.DataFrame(report).transpose().to_csv(f'{output_dir}/{model_name}_classification_report.csv')
+
+
+def store_model_roc_curve(model, test_data, test_labels, model_name, output_dir):
+    plt.figure(figsize=(15, 8))
+    ax = plt.gca()
+    plot_roc_curve(model, test_data, test_labels, ax=ax, alpha=0.8)
+    plt.savefig(f'{output_dir}/{model_name}_roc_curve.png', bbox_inches='tight')
+
+
+def compute_shapley_values(model_fitted, test_data, output_dir, model_name, train_data=None):
+    explainer = None
+    plt.figure(figsize=(20, 10))
+    if isinstance(model_fitted, GridSearchCV):
+        model_fitted = model_fitted.best_estimator_
+
+    if model_name in ['LogisticRegression', 'LinearRegression']:
+        masker = shap.maskers.Independent(data=train_data)
+        explainer = shap.LinearExplainer(model_fitted, masker=masker)
+    elif model_name in ['RandomForestClassifier']:
+        explainer = shap.TreeExplainer(model_fitted)
+    #else:
+    #    lg.info('Add your model type to tree, linear, gradient or deep explainer and add condition to this function')
+    shap_values = explainer.shap_values(test_data)
+    shap.summary_plot(shap_values, test_data, show=False)
+    plt.savefig(f'{output_dir}/{type(model_fitted).__name__}_shapley_values.png', bbox_inches='tight')
+
+
+def compute_features_importances(feature_names, coefficients, model_name, output_dir):
+    plt.figure(figsize=(20, 10))
+    safe_creation_directory(output_dir)
+    df_features_importances = pd.DataFrame(zip(feature_names, coefficients), columns=['feature', 'coefficient']).sort_values(by=['coefficient'], ascending=False)
+    #Plot Searborn bar chart
+    sns.barplot(x=df_features_importances['coefficient'], y=df_features_importances['feature'])
+    #Add chart labels
+    plt.title(model_name + 'FEATURE IMPORTANCE')
+    plt.xlabel('FEATURE IMPORTANCE')
+    plt.ylabel('FEATURE NAMES')
+    plt.savefig(f'{output_dir}/{model_name}_features_importances.png', bbox_inches='tight')
+
+"""
+def classification_report_image(true_labels, predictions, name_model, mode, output_path):
+    try:
+        lg.info(f'{name_model} results')
+        if mode == 'train':
+            lg.info(f'{mode} results')
+            return classification_report(true_labels, predictions)
+    except:
+        lg.info('Something went from')
+"""
