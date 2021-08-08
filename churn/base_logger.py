e6fa2355098670ab24c5548042ef3cc4d74d2934
damien <dmichelle@efficity.com>
2021-08-08 14:15:12 +0200

    clean base_logger.py file

--- churn/base_logger.py
+++ churn/base_logger.py
@@ -9,20 +9,3 @@ logging.basicConfig(
             level=logging.INFO,
             filemode='w',
             format='%(name)s - %(levelname)s - %(message)s')
-
-"""
-class BaseLogger:
-
-    def __init__(self):
-        self.logger = self.get_logger(self.__module__)
-
-    @staticmethod
-    def get_logger(name):
-        logging.basicConfig(
-            filename=os.path.join(os.path.dirname(__file__), './logs/churn_library.log'),
-            level=logging.INFO,
-            filemode='w',
-            format='%(name)s - %(levelname)s - %(message)s')
-        return logging.getLogger()
-"""
-
